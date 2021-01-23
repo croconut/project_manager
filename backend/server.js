37b5b23d6f0c019173ada7ec6a610d933ebb2ec9
croconut <marsteve@ucsc.edu>
2021-01-22 17:57:41 -0800

    server route update, starting session storage implementation
    * will need front end to read from front ends requested routes / apis in refactor
    *

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,92 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const helmet = require("helmet");
 const cors = require("cors");
 const path = require("path");
-const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
 const exercisesRouter = require("./routes/exercises");
 const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+let uri = "";
+if (process.env.NODE_ENV === "development") {
+  console.log("in development mode...");
+  uri = process.env.ATLAS_URI_DEV;
+} else if (process.env.NODE_ENV === "production") {
+  console.log("in production mode...");
+  uri = process.env.ATLAS_URI;
+} else {
+  console.log("testing mode");
+  uri = process.env.ATLAS_URI_TEST;
+}
+const cookieKey = process.env.SESSION_KEY;
+const cookieSecret = process.env.SESSION_SECRET;
+
+const sessionStoreOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const mongooseConnectionOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+let store = new MongoDBStore(
+  {
+    uri: uri,
+    collection: "sessions",
+    connectionOptions: sessionStoreOptions,
+  },
+  (err) => {
+    if (err) console.error("store connection error: " + err);
+  }
+);
+
+store.on("error", (err) => console.error("store error: " + err));
+
+app.use(
+  session({
+    key: cookieKey,
+    secret: cookieSecret,
+    resave: false,
+    saveUninitialized: false,
+    // one week in ms
+    cookie: { maxAge: 604800000 },
+    store: store,
+  })
+);
+
+mongoose.connect(uri, mongooseConnectionOptions);
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("mongo db database connection established successfully");
 });
 
-app.use("/api/exercises", exercisesRouter);
+app.use("/api/tasklist", exercisesRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get(["/", "/join", "/exercise/create", "/exercise/edit/:id"], (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
-});
+app.get(
+  ["/", "/join", "/tasklist/create", "/tasklist/edit/:id"],
+  (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+  }
+);
 
 app.listen(port, () => {
   console.log("server is running on port " + port);
