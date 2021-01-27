f95b33befea61d859004388993bc4f4a90e2314c
croconut <marsteve@ucsc.edu>
2021-01-26 21:49:14 -0800

    realized i wasn't destroying the session on the database

--- backend/routes/logout.js
+++ backend/routes/logout.js
@@ -4,9 +4,10 @@ require("dotenv").config();
 router.post("/", (req, res) => {
   // doubly ensure session does not persist in store
   req.session.user = {};
+  req.session.destroy(() =>
+    res.clearCookie(process.env.SESSION_KEY).redirect("/")
+  );
   // for some reason cookie only clears on redirect :p
-  res.clearCookie(process.env.SESSION_KEY)
-    .redirect("/");
 });
 
 module.exports = router;
