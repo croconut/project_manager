babb1aad9f12e373b2d9983257e53979ef9a2393
croconut <marsteve@ucsc.edu>
2021-01-31 15:01:00 -0800

    reformat to match testing setup scripts

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,17 @@ const app = require("./app/app");
 
 const startServer = async () => {
   const port = process.env.PORT || 5000;
-  const isTesting = process.env.NODE_ENV === "test";
   const appStore = await connect(app);
   const server = appStore.app.listen(port);
   const shutdownManager = createHttpTerminator({ server });
   // app gracefully shuts down
   const terminateHandle = async () => {
-    await shutdownManager.terminate();
-    await mongoose.connection.close();
-    await appStore.store.client.close();
-    process.exit();
+    const closeM = shutdownManager.terminate();
+    const closeS = appStore.store.client.close();
+    const close = mongoose.connection.close();
+    await close;
+    await closeM;
+    await closeS;
   };
   process.on("SIGTERM", terminateHandle);
   process.on("SIGINT", terminateHandle);
