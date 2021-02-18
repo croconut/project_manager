8c1b53d8c9c1dfb9cbad0f80313cb3585949be99
croconut <marsteve@ucsc.edu>
2021-02-17 23:47:03 -0800

    removing pointless code

--- frontend/src/components/Organization.tsx
+++ frontend/src/components/Organization.tsx
@@ -1,6 +1,5 @@
 import { makeStyles } from "@material-ui/core";
 import React, { FC } from "react";
-import GridPlus from "./helpers/GridPlus";
 
 const styles = makeStyles((theme) => ({
   gridPlus: {
@@ -8,24 +7,12 @@ const styles = makeStyles((theme) => ({
   },
   gridContainer: {
     width: "20%",
-  }
+  },
 }));
 
 const Organization: FC<any> = () => {
-  const data = [(<div>"hey"</div>), (<div>this</div>), (<div>is</div>), (<div>"hey"</div>)];
   const classes = styles();
-  return (
-    <GridPlus
-      className={classes.gridPlus}
-      justify="flex-end"
-      orderBy="column"
-      fillCount={3}
-      childClassName={classes.gridContainer}
-      childProps={{}}
-    >
-      {data}
-    </GridPlus>
-  );
+  return <div className={classes.gridPlus}>muh organization info</div>;
 };
 
 export default Organization;
