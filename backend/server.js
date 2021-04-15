36cad4dab848d834b1e916ec7c38e59d5a49a439
croconut <marsteve@ucsc.edu>
2021-04-15 16:24:06 -0700

    should be fully capable now
    * can re enable CSP to defaults when using https

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,26 @@ const connect = require("./app/connect");
 const { createHttpTerminator } = require("http-terminator");
 const mongoose = require("mongoose");
 const app = require("./app/app");
+const https = require("https");
+const fs = require("fs");
 
 const startServer = async () => {
   const port = process.env.PORT || 5000;
   const appStore = await connect(app);
-  const server = appStore.app.listen(port);
+  let server;
+  if (process.env.HTTPS === "true")
+    server = https
+      .createServer(
+        {
+          key: fs.readFileSync("./key.pem"),
+          cert: fs.readFileSync("./cert.pem"),
+        },
+        appStore.app
+      )
+      .listen(port);
+  else {
+    server = appStore.app.listen(port);
+  }
   const shutdownManager = createHttpTerminator({ server });
   // app gracefully shuts down
   const terminateHandle = async () => {
